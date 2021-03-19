5a087904735550e59a92fefbc28fe7ef720c5a5f
Haripriya-Perumal <80865888+Haripriya-Perumal@users.noreply.github.com>
2021-03-19 13:18:22 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java hi");  
-        System.out.println("Hello Java hi");  
+     System.out.println("Hell o Java hi");  
+        System.out.println("Hell o Java hi");  
     }  
 }  
